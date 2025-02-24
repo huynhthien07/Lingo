779df5aa733cf099ad5575238d4326a50a5a65ea
huynhthien07 <22521383@gm.uit.edu.vn>
2025-02-24 13:10:01 +0700

    hehe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
+import {
+  ClerkProvider
+} from '@clerk/nextjs';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const font = Nunito ({subsets:["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={font.className}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
