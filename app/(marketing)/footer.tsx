4458785645e0a4e467580048e81ed0f2e95600bc
Quach Chi Tai <22521285@gm.uit.edu.vn>
2025-03-02 12:19:50 +0700

    3/2/2024-p1
    (chạy lại nếu cần)
    npm install @radix-ui/react-slot
    npm install @clerk/nextjs
    npm install shadcn-ui@latest
    
    đổi mới:
    cũ: npx shadcn-ui@latest add sheet
    mới: npx shadcn@latest add sheet
    
    cập nhật lại phiên bản 1 số denpendence

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-2xl max-auto flex items-center
+            justify-evenly h-full">
+                <Button size='lg' variant="ghost" className="w-full">
+                    <Image
+                        src="/en.svg"
+                        alt="Logo"
+                        height={32} width={40}
+                        className="mr-4 rounded-md" />
+                    English
+                </Button>
+
+            </div>
         </footer>
     );
 };
--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -10,34 +10,34 @@ export const Header = () => {
             <div className="lg:max-w-screen-lg mx-auto flex items-center 
             justify-between h-full">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-                    <Image src = "/mascot.svg" height = {40} width = {40} alt = "Mascot"/>
+                    <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
                     <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
                         Lingo
                     </h1>
                 </div>
                 <ClerkLoading>
-                    <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
                 </ClerkLoading>
                 <ClerkLoaded>
                     <SignedIn>
                         <UserButton
-                        afterSignOutUrl="/"
+                            afterSignOutUrl="/"
                         />
                     </SignedIn>
                     <SignedOut>
                         <SignInButton
-                        mode="modal"
-                        afterSignInUrl="/learn"
-                        afterSignUpUrl="/learn"
+                            mode="modal"
+                            afterSignInUrl="/learn"
+                            afterSignUpUrl="/learn"
                         >
                             <Button>
                                 Sign in
                             </Button>
 
                         </SignInButton>
-                        
+
                     </SignedOut>
-                    
+
                 </ClerkLoaded>
             </div>
         </header>
