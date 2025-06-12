dc25de79f6dad3f4293866c1b3d9eb535492c601
Quach Chi Tai <22521285@gm.uit.edu.vn>
2025-06-12 07:09:53 +0700

    update
    Update packege

--- app/(main)/practises/page.tsx
+++ app/(main)/practises/page.tsx
@@ -8,7 +8,7 @@ const PracticesPage = async () => {
     return (
         <div className="h-full max-w-[912px] px-3 mx-auto">
             <h1 className="text-2xl font-bold text-neutral-700">
-                Available Tests
+                Practises
             </h1>
             <List tests={tests} />
         </div>
